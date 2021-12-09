964df0a04d90ab33e7de2440afe754038e072aae
McybearX <95560890+McybearX@users.noreply.github.com>
2021-12-09 13:17:48 +0700

    TTs.py
    Sip

---
+++ TTs.py
@@ -0,0 +1,211 @@
+#jika sudah selsai mengedit Tekan (CTRL + X lalu Y lalu ENTER)
+#coded by Mcybear×
+#Youtube Channel MBEWLEGS
+
+import time,os,sys
+
+os.system ('clear')
+
+def ahh(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./15)
+
+def aahh(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./70)
+
+def editgame():
+    cod()
+    ahh("""\x1b[1;91mPENTING!!!\x1b[1;93m
+ Dibutuhkan Keterampilan Menguasai Bahasa Python Untuk
+Mengedit Game Ini!
+\x1b[1;94m\x1b[1;91m\nJika tidak, \x1b[1;95m\x1b[1;91makan terjadi eror pada game !!!""") , time.sleep(1)
+    input ("\n\x1b[1;91m╔═\x1b[1;97mTekan enter untuk melanjutkan\n\x1b[1;91m╚═づ\x1b[1;94m[ENTER]")
+    os.system("nano TTs.py")
+    os.system("python TTs.py")
+
+def cod():
+    aahh("""\x1b[1;91m
+████████╗   ████████╗
+╚══██╔══╝   ╚══██╔══╝ \x1b[1;97m ██████
+\x1b[1;91m   ██║         ██║    \x1b[1;97m ██
+\x1b[1;91m   ██║         ██║    \x1b[1;97m ██████
+\x1b[1;91m   ██║         ██║    \x1b[1;97m     ██
+\x1b[1;91m   ╚═╝         ╚═╝    \x1b[1;97m ██████        """) , time.sleep(2)
+
+def show_m3nu():
+     aahh("""\n
+       \x1b[1;90m      ▄▄                     ▗▄▄▄▖▗▄▄▄▖
+           \x1b[1;91m █▀▀▌                    ▝▀█▀▘▝▀█▀▘
+           \x1b[1;92m▐▌    ▟██▖▐█▙█▖ ▟█▙        █    █  ▗▟██▖
+           \x1b[1;93m▐▌▗▄▖ ▘▄▟▌▐▌█▐▌▐▙▄▟▌       █    █  ▐▙▄▖▘
+           \x1b[1;94m▐▌▝▜▌▗█▀▜▌▐▌█▐▌▐▛▀▀▘       █    █   ▀▀█▖
+           \x1b[1;95m █▄▟▌▐▙▄█▌▐▌█▐▌▝█▄▄▌       █    █  ▐▄▄▟▌
+           \x1b[1;96m  ▀▀  ▀▀▝▘▝▘▀▝▘ ▝▀▀        ▀    ▀   ▀▀▀
+           \x1b[0;91m
+    ◆Coded by Mcybear×
+   🙏Suport YT MBEWLEGS""")
+     time.sleep(1)
+     m3nu()
+
+def m3nu():
+     print ("──────────────────────────────────────────────────────────────")
+     print ("\x1b[1;94m                     🎮 MULAI")
+     print ("                     ✉️  FAQ")
+     print ("                     💙 SUPORT")
+     print ("                     🔧 EDIT GAME\x1b[1;91m")
+     print ("──────────────────────────────────────────────────────────────")
+
+show_m3nu()
+show_m3nu = input("\x1b[1;93mPilih:\x1b[1;94m ")
+print ("\n")
+
+if show_m3nu == "mulai" or show_m3nu == "MULAI" :
+    print ('loading the Game....') , time.sleep(3)
+    os.system("clear")
+    print ('\x1b[1;97mKlik \x1b[1;91mSUBSCRIBE \x1b[1;97mUntuk Memulai Game')
+    time.sleep(3)
+    os.system("xdg-open https://youtube.com/c/MBEWLEGS")
+    input ("\x1b[1;91m╔═\x1b[1;97mKlik Enter Jika Sudah \x1b[1;91mSUBSCRIBE\n\x1b[1;91m╚═\x1b[1;94m[ENTER]")
+
+elif show_m3nu == "suport" or show_m3nu == "SUPORT" :
+    os.system("clear")
+    print ("Bayar Script ini dengan SUBSCRIBE Yt MBEWLEGS🙏") , time.sleep(3)
+    os.system("xdg-open https://youtube.com/c/MBEWLEGS")
+    print ("Terimakasih Telah Suport chanel MBEWLEG😉") , time.sleep(10)
+    os.system("python TTs.py")
+
+elif show_m3nu == "faq" or show_m3nu == "FAQ" :
+    os.system("clear")
+    ahh(""" \x1b[1;97mAssalamu'alaikum wr.wb 🙏
+Said developer :\x1b[1;91m\x1b[1;93m
+PENTING!!!\x1b[1;97m\x1b[1;93m
+ dilarang memperjual belikan game ini!!!
+kalo mau recode recode aja Tapi Jangan lo jual juga tod
+Hargai yg buat script.
+ \n\x1b[1;91mNote:
+ \x1b[1;97mGame ini hanya untuk Hiburan Semata
+Tidak untuk menyinggung siapapun
+mohon maaf jika ada kesamaan Nama,Peristiwa,TKP
+itu hanya kebetulan.
+                     o(〃＾▽＾〃)o
+\x1b[1;92mCara Main :\x1b[1;94m
+1.\x1b[1;97m pada kolom (\x1b[1;93mPilih :\x1b[1;97m )isi sesuai pilihan yg tertera
+  menggunakan huruf kecil
+\n\x1b[1;94m2. \x1b[1;97mpada menu terdapat 4 pilihan :
+     -MULAI (Untuk Memulai Game)
+     -FAQ (Untuk melihat pesan dari developer)
+     -SUPORT (Untuk mendukung developer lewat Channel
+              Youtubenya)
+     -EDIT GAME (Untuk mengedit Game ini sendiri)
+
+Terima kasih atas partisipasinya🙏
+Jangan lupa bersyukur atas semua yang diberikan Allah
+kepada kita:)
+
+saya akhiri wasallamu'alaikum wr.wb""") , time.sleep(2)
+    input ("\n\x1b[1;95mTekan Enter Untuk Kembali Ke Menu ")
+    os.system("python TTs.py")
+
+elif show_m3nu == "edit" or show_m3nu == "edit game":
+    os.system("clear")
+    editgame()
+
+else :
+    print ('Tidak ada pilihan')
+    time.sleep(2)
+    os.system("python TTs.py")
+
+#warna
+M = "\x1b[1;91m"
+def baner():
+     print ("""\x1b[1;91m╺┳╸╺┳╸┏━┓
+ \x1b[1;97m┃\x1b[1;97m  ┃ ┗━┓
+ ╹  ╹ ┗━┛""")
+     print (M)
+     print ('──────────────────────────────────────────────────────────────')
+     print ('\x1b[1;95m😈PILIHLAH JAWABAN  dibawah ⬇ ini dengan BENAR!!!\n',M)
+     print ('*\x1b[1;97mAuthor   : Mcybear×')
+     print ('\x1b[1;91m*\x1b[1;97mFacebook : https://m.facebook.com/Mcybear\n')
+     print ('\x1b[1;91m#\x1b[1;97mIsi Jawaban Menggunakan Huruf Kecil!!!',M)
+     print ('──────────────────────────────────────────────────────────────')
+def Soal1():
+     time.sleep(1)
+     ahh("""\x1b[1;94m[Stage1]\n
+\x1b[1;97m  Apa nama wadah untuk menanam tanaman hias... \x1b[1;91m?\n
+ \x1b[1;94mA.\x1b[1;97mMangkok
+ \x1b[1;94mB.\x1b[1;97mJamban
+ \x1b[1;94mC.\x1b[1;97mPot""")
+
+     soal1 = input ('\n\x1b[1;93mPilih:\x1b[1;94m ')
+
+     if soal1 == "c" or soal1 == "pot":time.sleep(1) , print ('\x1b[1;92m✔ ') , time.sleep(1) , print ('\x1b[1;94mBener Anjir😆') , satu()
+
+     else :
+         print ('\x1b[1;31mSalah Tood🙄') , time.sleep(3)
+         satu()
+
+def satu():
+     satu = input ('\x1b[1;97m [\x1b[1;95mUlang\x1b[1;97m],[\x1b[1;95mMenu\x1b[1;97m],\x1b[1;97m[\x1b[1;95mLanjut\x1b[1;97m]\n • \x1b[1;95m')
+
+     if satu == "menu":
+         print ('loading...')
+         time.sleep(2)
+         os.system('python TTs.py')
+     elif satu == "lanjut":
+         print ('loading...')
+         time.sleep(2) , os.system('clear')
+         baner()
+         Soal2()
+     elif satu == "ulang":
+         print ('loading...')
+         time.sleep(2) , os.system("clear")
+         baner()
+         Soal1()
+     else :
+         print ('Tidak ada pilihan!!!')
+         clear
+         satu()
+
+def Soal2():
+     ahh("""\x1b[1;94m[Stage2]\n
+\x1b[1;97m  Apa yg lo lakuin kalo ngeliat pacarlo
+lagi jalan bareng kawan lo sendiri... \n
+ \x1b[1;94mA.\x1b[1;97mMarah gajelas ke cewe lo
+ \x1b[1;94mB.\x1b[1;97mAjak gelud kawan lo
+ \x1b[1;94mC.\x1b[1;97mB aja,bisa cari cewe laen""")
+
+     soal2 = input ('\n\x1b[1;93mPilih:\x1b[1;94m ')
+
+     if soal2 == "c" or soal2 == "pot":time.sleep(1) , print ('\x1b[1;92m✔ ') , time.sleep(1) , print ('\x1b[1;94mBener Anjir😆')
+
+     else :
+         print ('\x1b[1;31mSalah Tood🙄') , time.sleep(3)
+         lipih()
+
+def lipih():
+     lipih = input ('\x1b[1;97m [\x1b[1;95mUlang\x1b[1;97m],[\x1b[1;95mMenu\x1b[1;97m],\x1b[1;97m[\x1b[1;95mLanjut\x1b[1;97m]\n • \x1b[1;95m')
+
+     if lipih == "menu":
+         print ('loading...')
+         time.sleep(2)
+         os.system('python TTs.py')
+     elif lipih == "lanjut":
+         print ('loading...')
+         time.sleep(2)
+elif lipih == "ulang":
+         print ('loading...')
+         time.sleep(2) , os.system("clear")
+         baner()
+         Soal2()
+     else :
+         print ('Tidak ada pilihan!!!')
+         Soal2()
+
+os.system('clear')
+baner()
+Soal1()
